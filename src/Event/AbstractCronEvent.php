b682438c32c231a4cb4cbcc95f369b561561aa32
willemverspyck <willemverspyck@users.noreply.github.com>
2024-11-19 18:37:14 +0100

    Update events

--- src/Event/AbstractCronEvent.php
+++ src/Event/AbstractCronEvent.php
@@ -5,18 +5,25 @@ declare(strict_types=1);
 namespace Spyck\AutomationBundle\Event;
 
 use Spyck\AutomationBundle\Cron\CronInterface;
+use Spyck\AutomationBundle\Entity\Cron;
+use Spyck\AutomationBundle\Entity\ModuleInterface;
 use Spyck\AutomationBundle\Parameter\ParameterInterface;
 use Symfony\Contracts\EventDispatcher\Event;
 
 abstract class AbstractCronEvent extends Event
 {
-    public function __construct(private readonly CronInterface $cron, private readonly ParameterInterface $parameter)
+    public function __construct(private readonly CronInterface $job, private readonly ParameterInterface $parameter)
     {
     }
 
-    public function getCron(): CronInterface
+    public function getModule(): ModuleInterface
     {
-        return $this->cron;
+        return $this->job->getAutomationModule();
+    }
+
+    public function getCron(): Cron
+    {
+        return $this->job->getAutomationCron();
     }
 
     public function getParameter(): ParameterInterface
--- src/Event/AbstractTaskEvent.php
+++ src/Event/AbstractTaskEvent.php
@@ -4,19 +4,20 @@ declare(strict_types=1);
 
 namespace Spyck\AutomationBundle\Event;
 
+use Spyck\AutomationBundle\Entity\ModuleInterface;
 use Spyck\AutomationBundle\Parameter\ParameterInterface;
 use Spyck\AutomationBundle\Task\TaskInterface;
 use Symfony\Contracts\EventDispatcher\Event;
 
 abstract class AbstractTaskEvent extends Event
 {
-    public function __construct(private readonly TaskInterface $task, private readonly ParameterInterface $parameter)
+    public function __construct(private readonly TaskInterface $job, private readonly ParameterInterface $parameter)
     {
     }
 
-    public function getTask(): TaskInterface
+    public function getModule(): ModuleInterface
     {
-        return $this->task;
+        return $this->job->getAutomationModule();
     }
 
     public function getParameter(): ParameterInterface
