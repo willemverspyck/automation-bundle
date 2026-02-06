01e2afe5af4da66badb1ade81c6a3620181fc907
Willem Verspyck <willemverspyck@users.noreply.github.com>
2026-02-06 11:38:57 +0100

    Update command

--- src/Command/TaskCommand.php
+++ src/Command/TaskCommand.php
@@ -6,22 +6,23 @@ namespace Spyck\AutomationBundle\Command;
 
 use Exception;
 use RuntimeException;
+use Spyck\AutomationBundle\Event\TaskEvent;
 use Spyck\AutomationBundle\Repository\ModuleRepository;
-use Spyck\AutomationBundle\Service\TaskService;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Attribute\Option;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Completion\CompletionInput;
 use Symfony\Component\Console\Completion\CompletionSuggestions;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 #[AsCommand(name: 'spyck:automation:task', description: 'Execute task for module')]
 final class TaskCommand extends Command
 {
-    public function __construct(private readonly ModuleRepository $moduleRepository, private readonly TaskService $taskService)
+    public function __construct(private readonly EventDispatcherInterface $eventDispatcher, private readonly ModuleRepository $moduleRepository)
     {
         parent::__construct();
     }
@@ -35,12 +36,36 @@ final class TaskCommand extends Command
         }
     }
 
-    protected function configure(): void
+    /**
+     * @throws Exception
+     */
+    public function __invoke(SymfonyStyle $style, #[Option(name: 'moduleId')] ?int $moduleId = null, #[Option(name: 'variableKey')] array $variableKeys = [], #[Option(name: 'variableValue')] array $variableValues = []): int
     {
-        $this
-            ->addOption('moduleId', null, InputOption::VALUE_REQUIRED, 'The "id" of the module?')
-            ->addOption('variableKey', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'Variable key')
-            ->addOption('variableValue', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'Variable value');
+        $module = $this->moduleRepository->getModuleById($moduleId);
+
+        if (null === $module) {
+            $style->error(sprintf('Module "%d" not found.', $moduleId));
+
+            return Command::FAILURE;
+        }
+
+        if (count($variableKeys) !== count($variableValues)) {
+            $style->error('Parameter "variableKey" and "variableValue" must be equal.');
+
+            return Command::FAILURE;
+        }
+
+        $variables = array_combine($variableKeys, $variableValues);
+
+        $style->info('Execute task');
+
+        $taskEvent = new TaskEvent($module, $variables);
+
+        $this->eventDispatcher->dispatch($taskEvent);
+
+        $style->info('Done');
+
+        return Command::SUCCESS;
     }
 
     protected function interact(InputInterface $input, OutputInterface $output): void
@@ -66,34 +91,6 @@ final class TaskCommand extends Command
         $answer = $this->getHelper('question')->ask($input, $output, $question);
 
         $input->setOption('moduleId', $answer);
-    }
-
-    /**
-     * @throws Exception
-     */
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $style = new SymfonyStyle($input, $output);
-
-        $moduleId = (int) $input->getOption('moduleId');
-        $variableKeys = $input->getOption('variableKey');
-        $variableValues = $input->getOption('variableValue');
-
-        if (count($variableKeys) !== count($variableValues)) {
-            $style->error('Parameter "variableKey" and "variableValue" must be equal.');
-
-            return Command::FAILURE;
-        }
-
-        $variables = array_combine($variableKeys, $variableValues);
-
-        $output->writeln('Execute task');
-
-        $this->taskService->executeTaskAsMessage($moduleId, $variables);
-
-        $output->writeln('Done');
-
-        return Command::SUCCESS;
     }
 
     private function getModules(): array
